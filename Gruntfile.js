90d64a6961d73df6d63544d8f62c2c2285be7710
David <xudavidb@gmail.com>
2015-05-05 23:13:21 -0700

    remove client grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     broccoli: {
-      client: {
-        dest: 'dist',
-        config: 'brocfiles/client.js'
-      },
       src: {
         dest: 'lib',
         config: 'brocfiles/src.js'
@@ -18,8 +14,4 @@ module.exports = function(grunt) {
   grunt.registerTask('watch', ['broccoli:src:watch'])
   grunt.registerTask('build', ['broccoli:src:build'])
   grunt.registerTask('serve', ['broccoli:src:serve'])
-
-  grunt.registerTask('clientw', ['broccoli:client:watch'])
-  grunt.registerTask('clientb', ['broccoli:client:build'])
-  grunt.registerTask('clients', ['broccoli:client:serve'])
 }
