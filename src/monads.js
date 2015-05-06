eb09beff28ae3e9f1d1cf1ab980b954d5560c3db
David <xudavidb@gmail.com>
2015-05-05 23:12:57 -0700

    adjust spacing and fix checkType

--- src/monads.js
+++ src/monads.js
@@ -1,49 +1,53 @@
 var TypeError = "TypeError";
 
-function checkType(obj) {
-    if (!(obj instanceof (typeof this))){
-        throw TypeError;
-    }
-    return obj;
+function checkType(context, obj) {
+  if (!(obj instanceof context.constructor)){
+    throw TypeError;
+  }
+  return obj;
 }
 
 function Identity(value) {
-    this.value = value;
+  this.value = value;
 }
 
 Identity.prototype.pipe = function(transform) {
-    return checkType(transform(this.value));
+  return checkType(this, transform(this.value));
 };
-
 
 // Maybe
 function Maybe(value) {
-    if (!(this instanceof Maybe)) return new Maybe(value);
-    this.value = value;
+  if (!(this instanceof Maybe)) return new Maybe(value);
+  this.value = value;
 }
 
 function Nothing() {
-    return new Maybe(null);
+  return new Maybe(null);
 }
 
 Maybe.prototype.pipe = function(transform) {
-    if (this.value == null || this.value == undefined) {
-        return Nothing();
-    }
+  if (this.value == null || this.value == undefined) {
+    return Nothing();
+  }
 
-    return checkType(transform(this.value));
+  return checkType(transform(this.value));
 };
 
 Maybe.prototype.inspect = function() {
-    return this.value;
+  return this.value;
 }
 
 // list
 Array.prototype.pipe = function (transform) {
-    var results = [];
-    for (var i = 0; i < this.length; i++) {
-        result = transform(this[i]);
-        results.push(result);
-    }
-    return results;
+  var results = [];
+  for (var i = 0; i < this.length; i++) {
+    result = transform(this[i]);
+    results.push(result);
+  }
+  return results;
 };
+
+module.exports = {
+  Identity: Identity,
+  Array: Array
+}
