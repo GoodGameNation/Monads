7dc35117092e6b05ebd19d68ec4106b47bf893ec
Yunxing Dai <yunxing@cloudwalk.me>
2015-05-08 21:50:49 -0700

    add test

--- test/test.js
+++ test/test.js
@@ -6,10 +6,45 @@ describe('Monads Test', function() {
   describe('Identity Monad', function() {
       it('Test the Identity Monad on an addOne function', function() {
         identity = new Identity(1)
-        addOne = function(value) {
+        var addOne = function(value) {
           return new Identity(value+1)
         }
         identity.pipe(addOne).value.should.equal(2)
-        })
+      })
+
+      it('Test the Maybe Monad on an addOne function', function() {
+        maybe = Just(1)
+        var addOne = function(value) {
+          return Just(value+1)
+        }
+
+        var toNothing = function(value) {
+          return Nothing()
+        }
+        maybe.pipe(addOne).value.should.equal(2)
+        maybe.pipe(addOne).pipe(toNothing).pipe(addOne).value.should.equal(null)
+      })
+    })
+})
+
+describe('Functor Test', function() {
+  describe('Identity Functor', function() {
+      it('Test the Identity Functor on an addOne function', function() {
+        identity = new Identity(1)
+        var addOne = function(value) {
+          return value+1
+        }
+        identity.fmap(addOne).value.should.equal(2)
+      })
+
+      it('Test the Maybe functor on an addOne function', function() {
+        maybe = Just(1)
+        var addOne = function(value) {
+          return value+1
+        }
+        maybe.fmap(addOne).value.should.equal(2)
+        maybe = Nothing()
+        maybe.fmap(addOne).value.should.equal(null)
+      })
     })
 })
