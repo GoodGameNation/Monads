0268ab6d87d3845215d5cfe4e9fe724ce85824a8
David <xudavidb@gmail.com>
2015-05-08 22:22:56 -0700

    fix test to use proper should

--- test/test.js
+++ test/test.js
@@ -7,47 +7,47 @@ var Monads = require('../lib/monads.js')
 
 describe('Monads Test', function() {
   describe('Identity Monad', function() {
-      it('Test the Identity Monad on an addOne function', function() {
-        identity = Identity(1)
-        var addOne = function(value) {
-          return Identity(value+1)
-        }
-        identity.pipe(addOne).value.should.equal(2)
-      })
-
-      it('Test the Maybe Monad on an addOne function', function() {
-        maybe = Just(1)
-        var addOne = function(value) {
-          return Just(value+1)
-        }
-
-        var toNothing = function(value) {
-          return Nothing()
-        }
-        maybe.pipe(addOne).value.should.equal(2)
-        maybe.pipe(addOne).pipe(toNothing).pipe(addOne).value.should.equal(null)
-      })
+    it('Test the Identity Monad on an addOne function', function() {
+      identity = Identity(1)
+      var addOne = function(value) {
+        return Identity(value+1)
+      }
+      identity.pipe(addOne).value.should.equal(2)
     })
+
+    it('Test the Maybe Monad on an addOne function', function() {
+      maybe = Just(1)
+      var addOne = function(value) {
+        return Just(value+1)
+      }
+
+      var toNothing = function(value) {
+        return Nothing()
+      }
+      maybe.pipe(addOne).value.should.equal(2)
+      should.not.exist(maybe.pipe(addOne).pipe(toNothing).pipe(addOne).value)
+    })
+  })
 })
 
 describe('Functor Test', function() {
   describe('Identity Functor', function() {
-      it('Test the Identity Functor on an addOne function', function() {
-        identity = Identity(1)
-        var addOne = function(value) {
-          return value+1
-        }
-        identity.fmap(addOne).value.should.equal(2)
-      })
-
-      it('Test the Maybe functor on an addOne function', function() {
-        maybe = Just(1)
-        var addOne = function(value) {
-          return value+1
-        }
-        maybe.fmap(addOne).value.should.equal(2)
-        maybe = Nothing()
-        maybe.fmap(addOne).value.should.equal(null)
-      })
+    it('Test the Identity Functor on an addOne function', function() {
+      identity = Identity(1)
+      var addOne = function(value) {
+        return value+1
+      }
+      identity.fmap(addOne).value.should.equal(2)
     })
+
+    it('Test the Maybe functor on an addOne function', function() {
+      maybe = Just(1)
+      var addOne = function(value) {
+        return value+1
+      }
+      maybe.fmap(addOne).value.should.equal(2)
+      maybe = Nothing()
+      should.not.exist(maybe.fmap(addOne).value)
+    })
+  })
 })
